642a48749f6f65c1dfa522fc49ec36959ffd05ce
ogaharu <ogaharu8pureko@gmail.com>
2018-04-05 14:10:35 +0900

    アクションシーン追加

---
+++ CCMacho/Assets/C#/ZMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(0f,0f,transform.position.z + 1f);
+	}
+}
